efbdb78f632aeeee4508b8c7b42925266dba19d6
DymOK93 <dym93ok@yandex.ru>
2021-08-01 03:28:02 +0300

    First version

---
+++ driver.cpp
@@ -0,0 +1,50 @@
+﻿#include "driver.h"
+
+#include <string.hpp>
+
+#include <fmt/compile.h>
+#include <fmt/xchar.h>
+
+using namespace ktl;
+
+template <class ConcreteContext>
+struct Context {
+  static constexpr size_t SIZE = sizeof(ConcreteContext);
+};
+
+EXTERN_C NTSTATUS DriverEntry([[maybe_unused]] PDRIVER_OBJECT DriverObject,
+                              [[maybe_unused]] PUNICODE_STRING RegistryPath) {
+  try {
+    const auto time{GetCurrentTime()};
+    auto str{format(L"[{:02}-{:02}-{:04} {:02}:{:02}:{:02}.{:03}][{}]",
+                    time.Day, time.Month, time.Year, time.Hour, time.Minute,
+                    time.Second, time.Milliseconds, L"CoroDriverSample")};
+    DbgPrint("%wZ\n", str.raw_str());
+    system_thread thread;
+    resuming_on_new_thread(thread);
+    thread.join();
+  } catch (const exception& exc) {
+    DbgPrint("Unhandled exception caught: %wS with code %x\n", exc.what(),
+             exc.code());
+    return exc.code();
+  }
+  return STATUS_INSUFFICIENT_RESOURCES;
+}
+
+task resuming_on_new_thread(system_thread& out) {
+  DbgPrint("Coroutine started on thread: %u\n",
+           HandleToUlong(PsGetCurrentThreadId()));
+  co_await switch_to_new_thread(out);
+  DbgPrint("Coroutine resumed on thread: %u\n",
+           HandleToUlong(PsGetCurrentThreadId()));
+}
+
+TIME_FIELDS GetCurrentTime() noexcept {
+  const auto current_time{
+      chrono::system_clock::now().time_since_epoch().count()};
+  LARGE_INTEGER native_time;
+  native_time.QuadPart = static_cast<long long>(current_time);
+  TIME_FIELDS time_fields;
+  RtlTimeToTimeFields(addressof(native_time), addressof(time_fields));
+  return time_fields;
+}
---
+++ driver.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <bugcheck.h>
+#include <exception.h>
+#include <thread.h>
+#include <coroutine.hpp>
+
+auto switch_to_new_thread(ktl::system_thread& out) {
+  using ktl::system_thread;
+
+  struct awaitable {
+    bool await_ready() { return false; }
+    void await_suspend(ktl::coroutine_handle<> other) {
+      ktl::throw_exception_if<ktl::runtime_error>(out_th->joinable(),
+                                                  L"output thread is busy",
+                                                  ktl::constexpr_message_tag{});
+      *out_th = system_thread([other] { other.resume(); });
+    }
+    void await_resume() {}
+
+    system_thread* out_th;
+  };
+
+  return awaitable{ktl::addressof(out)};
+}
+
+struct task {
+  struct promise_type {
+    task get_return_object() { return {}; }
+    ktl::suspend_never initial_suspend() { return {}; }
+    ktl::suspend_never final_suspend() noexcept { return {}; }
+    void return_void() {}
+    void unhandled_exception() {}
+  };
+};
+
+task resuming_on_new_thread(ktl::system_thread& out);
+
+TIME_FIELDS GetCurrentTime() noexcept;
